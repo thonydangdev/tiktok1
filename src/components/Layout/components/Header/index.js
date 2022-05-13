19c5f40398b52a4a14c31a86c907c07bb1833580
thonydangdev <thongdang4298@gmail.com>
2022-05-13 16:07:42 +0700

    headerUI1

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,12 +1,32 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
-
+import images from '~/assets/images';
+import React from 'react';
+import { FontAwesomeIcon, spinPulse } from '@fortawesome/react-fontawesome';
+import { faXmarkCircle, faMagnifyingGlass, faEllipsisVertical, faSpinner } from '@fortawesome/free-solid-svg-icons';
 const cx = classNames.bind(styles);
+console.log(images);
 
 function Header() {
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
+            <div className={cx('inner')}>
+                <img src={images.logo} alt="Tiktok" />
+                <div className={cx('search')}>
+                    <input placeholder="Search this account" type="text" />
+                    <button className={cx('close')}>
+                        <FontAwesomeIcon icon={faXmarkCircle} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('actions')}>
+                    <FontAwesomeIcon icon={faEllipsisVertical} />
+                </div>
+            </div>
         </header>
     );
 }
